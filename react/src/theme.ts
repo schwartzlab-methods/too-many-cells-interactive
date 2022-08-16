3ab7400d9e884a113173b785065590b28860d235
cklamann <12862284+cklamann@users.noreply.github.com>
2022-08-16 12:37:53 -0400

    move theme to separate module

---
+++ react/src/theme.ts
@@ -0,0 +1,17 @@
+const theme = {
+    palette: {
+        grey: '#5e6365',
+        lightGrey: '#d1d9dd',
+        primary: '#009FFD',
+        primaryDark: '#00426a',
+        secondary: '#FFA400',
+        text: '#000000',
+        white: '#ffffff',
+    },
+    breakpoints: {
+        lg: '1200px',
+        md: '900px',
+    },
+};
+
+export default theme;
